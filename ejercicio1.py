1226351a862fa12b3f63aa6ab4bb98e7acc9b582
José Ruiz <jcrcsh@gmail.com>
2016-06-09 07:46:44 -0500

    1.1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,5 +1,5 @@
 print("Multiplos de un numero")
 numero = int(input("Ingrese un numero: "))
-for i range(numero)
-    numero = numero * i
-    // imprime el numero
+for i in range(10):
+    suma = suma + numero
+    print (suma)
