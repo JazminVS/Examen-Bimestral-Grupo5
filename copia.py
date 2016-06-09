a4fc0b135beb59109c456a858a56a89bfcf4f368
José Ruiz <jcrcsh@gmail.com>
2016-06-09 08:28:26 -0500

    1

--- copia.py
+++
@@ -1,9 +0,0 @@
-print("Multiplos de un numero")
-numeroI = int(input("Ingrese un numero: "))
-numeroM = numeroI
-numeroF = 0
-for i in range(1, 1000):
-    print (numeroI)
-    numeroI = numeroI + numeroM
-    if numeroI > 1000:
-        exit()
