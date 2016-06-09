226942bc6e42a20f113f682d333ab80738222e3b
José Ruiz <jcrcsh@gmail.com>
2016-06-09 09:26:41 -0500

    menu 1 2

--- menu.py
+++ menu.py
@@ -1,9 +1,38 @@
+
+def creartxt():
+	archi=open('NumeroPrimo.txt', 'w')
+	archi.close()
+def grabartxt(num):
+	archi=open('NumeroPrimo.txt', 'a')
+	archi.write(str(num)+"\t")
+	archi.close()
+
+def primo(num, n):
+	a = 0
+	for i in range(1, n +1):
+		if(num % i == 0):
+		a = a + 1
+	if(a == 2):
+	print("es primo: " +str(num))
+	return num
+
+def primo1(num, n):
+	a = 0
+	for i in range(1, n +1):
+	if(num % i == 0):
+		a = a + 1
+	if(a == 2):
+		return num
+
 def ejercicio1():
 	print("1. N numeros primos")
-	
-
-
-
+	creartxt()
+	print("Calcular n primos")
+	numero = int(input("Ingrese un numero: "))
+	for i in range(1, numero):
+		primo(i, numero)
+		aux=primo1(i,numero)
+		grabartxt(aux)
 
 def creartxt():
 	archi=open('Multiplo.txt', 'w')
