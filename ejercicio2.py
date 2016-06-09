107e7b896c9915dddea55ecb4af63e0cdf5fe992
ricardosinchiguano1995 <rikysdjs1995@hotmail.es>
2016-06-09 08:34:41 -0500

    2.2
    guardando en texto

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,9 +1,19 @@
+def creartxt():
+	archi=open('NumeroPrimo.txt', 'w')
+	archi.close()
+def grabartxt(num):
+	archi=open('NumeroPrimo.txt', 'a')
+	archi.write(str(num))
+	archi.close()
+
+creartxt()
 print("Multiplos de un numero")
 numeroI = int(input("Ingrese un numero: "))
 numeroM = numeroI
 numeroF = 0
 for i in range(1, 1000):
     print (numeroI)
+    grabartxt(numeroI)
     numeroI = numeroI + numeroM
     if numeroI > 1000:
         exit()
