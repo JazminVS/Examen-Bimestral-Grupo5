923aacdb638cf43c8430c21b178b47967f2665cd
José Ruiz <jcrcsh@gmail.com>
2016-06-09 09:21:26 -0500

    1.9

--- menu.py
+++ menu.py
@@ -1,7 +1,32 @@
 def ejercicio1():
 	print("1. N numeros primos")
+	
+
+
+
+
+def creartxt():
+	archi=open('Multiplo.txt', 'w')
+	archi.close()
+	
+def grabartxt(num):
+	archi=open('Multiplo.txt', 'a')
+	archi.write(str(num)+"\t")
+	archi.close()
+
 def ejercicio2():
 	print("2. N multiplos de un numero")
+	creartxt()
+	print("Multiplos de un numero")
+	numeroI = int(input("Ingrese un numero: "))
+	numeroM = numeroI
+	numeroF = 0
+	for i in range(1, 1000):
+		print (numeroI)
+		grabartxt(numeroI)
+		numeroI = numeroI + numeroM
+		if numeroI > 1000:
+			exit()
 
 def ejercicio3():
 	print("3. Contar palabras")
