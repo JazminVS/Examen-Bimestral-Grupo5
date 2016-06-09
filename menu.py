f0e4d230b83fafe5e12c6665616d9addb1f879df
José Ruiz <jcrcsh@gmail.com>
2016-06-09 09:14:17 -0500

    menu

---
+++ menu.py
@@ -0,0 +1,43 @@
+def ejercicio1():
+	print("1. N numeros primos")
+def ejercicio2():
+	print("2. N multiplos de un numero")
+
+def ejercicio3():
+	print("3. Contar palabras")
+
+def ejercicio4():
+	print("4. Traductor")
+
+def ejercicio5():
+	print("5. Area y volumen de una esfera")
+
+def ejercicio6():
+	print("6. Tablero raro de ajedrez")
+
+
+print("ESCUELA POLITECNICA NACIONAL")
+print("ESCUELA DE FORMACION DE TECNOLOGOS")
+print("\nPROGRAMACION AVANZADA")
+print("1. N numeros primos")
+print("2. N multiplos de un numero")
+print("3. Contar palabras")
+print("4. Traductor")
+print("5. Area y volumen de una esfera")
+print("6. Tablero raro de ajedrez")
+print("7. Salir")
+opcion = int(input("Elija una opcion: "))
+if opcion == 1:
+	ejercicio1()
+if opcion == 2:
+	ejercicio2()
+if opcion == 3:
+	ejercicio3()
+if opcion == 4:
+	ejercicio4()
+if opcion == 5:
+	ejercicio5()
+if opcion == 6:
+	ejercicio6()
+if opcion == 7:
+	exit()
