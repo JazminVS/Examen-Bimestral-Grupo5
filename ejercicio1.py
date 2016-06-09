6c1281ddb464a12f12fa5b1b62d2fa681ab333e7
ricardosinchiguano1995 <rikysdjs1995@hotmail.es>
2016-06-09 08:23:18 -0500

    1.1
    LA FUNCION SOLO PARA SABER SI ES UN NUMERO PRIMO

--- ejercicio1.py
+++ ejercicio1.py
@@ -22,3 +22,16 @@ else:
    print("es primo 2, 3")
  else:
   print("ingrese un numero positivo")
+
+//FUNCION PARA DETMINAR SI UN NUMERO ES UN PRIMO 
+def primo(num):
+  a=0
+  for i range(1, n+1):
+    if(num%i==0):
+      a=a+1
+    if(a!=2):
+      print("No es primo")
+    else:
+      print("es primo")
+
+
