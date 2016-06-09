75cd83a28efea24007beb9d9dcae43c5d6553b0f
José Ruiz <jcrcsh@gmail.com>
2016-06-09 08:29:50 -0500

    1.6

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,29 +1,4 @@
-﻿n, cont = 4, 2
-num = int(input("Ingrese un numero: "))
-if(num > 2):
- cad = "2 - 3"
- while cont < num:
-  i=2
-  while i<=num:
-   if(i==n):
-    cad= cad+" - "+str(i)
-    cont=cont+1
-   else:
-    if(n % i ==0):
-     i=n
-   i=i+1
-  n=n+1
- print(cad)
-else:
- if(num>0):
-  if(num==1):
-   print("es primo 2")
-  else:
-   print("es primo 2, 3")
- else:
-  print("ingrese un numero positivo")
-
-//FUNCION PARA DETMINAR SI UN NUMERO ES UN PRIMO 
+﻿
 def primo(num):
   a=0
   for i range(1, n+1):
