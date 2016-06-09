a102d73e6514ce5f87a7088151d15e9023742cac
José Ruiz <jcrcsh@gmail.com>
2016-06-09 08:53:44 -0500

    1.8

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,12 +1,14 @@
 ﻿
-def primo(num):
-  a=0
-  for i range(1, n+1):
-    if(num%i==0):
-      a=a+1
-    if(a!=2):
-      print("No es primo")
-    else:
-      print("es primo")
+def primo(num, n):
+  a = 0
+  for i in range(1, n +1):
+    if(num % i == 0):
+      a = a + 1
+  if(a == 2):
+    print("es primo: " +str(num))
+    
 
-
+print("Calcular n primos")
+numero = int(input("Ingrese un numero: "))
+for i in range(1, numero):
+  primo(i, numero)
--- ejercicio3.py
+++ ejercicio3.py
@@ -3,9 +3,9 @@ from tkinter import messagebox
 root=Tk()
 root.title("La conjura de los necios")
 root.geometry("800x800+450+450")
- 
+
 def Call():
-    archivo=open("LaConjuradelosNecios.txt","r")
+    archivo=open("archivo.txt","r")
     cont=archivo.read()
     msg=Message(root, text =cont)
     msg.config(bg="lightblue", font=("times",10,"italic"))
@@ -15,14 +15,8 @@ def Numpalabras ():
     archivo=open("LaConjuradelosNecios.txt","r")
     cont=archivo.read()
     messagebox.showinfo("test","ARCHIVO TXT (#DE PALABRAS): " + str(len(cont.split(" "))))
-    
-
-    
-
+  
 numpala = Button(root, text = "LEER NUM.PALABRAS", command = Numpalabras, width=15)
 numpala.grid(row=4,column=2)
 
-
-
- 
 root.mainloop()
